68317bddf1c671d1ee90d9946247614a2f0b01eb
chalbin73 <chalbin73@gmail.com>
2022-11-13 17:45:45 +0100

    Setting right version, Patch ++, added windows support for 0.2

--- include/static_infos.h
+++ include/static_infos.h
@@ -10,8 +10,8 @@
 //Version shall be updated for each release (binary release on github)
 #define VER_MAJOR 0 //Non backwards compatible changes
 #define VER_MINOR 2 //Backwards compatible changes
-#define VER_PATCH 0 //Patches
+#define VER_PATCH 1 //Patches
 
-#define VERSION_TEXT "0.2.0"
+#define VERSION_TEXT "0.2.1"
 
 #endif //__STATIC_INFOS_H__
